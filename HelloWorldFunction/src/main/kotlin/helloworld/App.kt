b404f554cee3a60ca4daa5bb5f7241c6bd0d4dbf
Philip Yurchuk <philip.yurchuk@madeupname.com>
2022-09-08 00:11:09 -0700

    samkgg-3 Add logging Updated app for AWS Lambda Powertools logging.
    build.gradle
    Added Powertools dependencies, which required lowering JVM target from 17 to 16 due to AspectJ limitation.
    
    reflect-config.json, resource-config.json
    Adding log4j2 makes GraalVM complain loudly. You have to add a lot to those files to get it to run without crashing.
    Followed example here: https://github.com/muellerc/graalvm-sandbox/tree/main/log4j2-sandbox/src/main/resources/META-INF/native-image/com.amazonaws.sample.cmr.sns-throughput-optimization/log4j2-sandbox
    From this thread: https://github.com/oracle/graal/issues/2008

--- HelloWorldFunction/src/main/kotlin/helloworld/App.kt
+++ HelloWorldFunction/src/main/kotlin/helloworld/App.kt
@@ -4,12 +4,18 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse
+import org.apache.logging.log4j.LogManager
+import software.amazon.lambda.powertools.logging.Logging
 
 /**
  * Handler for requests to Lambda function.
  */
 class App : RequestHandler<APIGatewayV2HTTPEvent?, APIGatewayV2HTTPResponse> {
+
+    var log = LogManager.getLogger()
+    @Logging
     override fun handleRequest(input: APIGatewayV2HTTPEvent?, context: Context?): APIGatewayV2HTTPResponse {
+        log.info("Entered handleRequest.")
         val headers: MutableMap<String, String> = HashMap()
         headers["Content-Type"] = "application/json"
         headers["X-Custom-Header"] = "application/json"
