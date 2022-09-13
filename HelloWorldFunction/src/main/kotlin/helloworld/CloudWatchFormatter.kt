c1f0f3f8a42fd3c0643301b4cfa558a5f4f3c99e
Philip Yurchuk <philip.yurchuk@madeupname.com>
2022-09-13 14:23:19 -0700

    samkgg-4 Add java.util.logging Basic working example. template.yaml adds LOGGER_REPLACE_NEWLINES env var build.gradle adds SLF4J JUL lib, sets LOGGER_REPLACE_NEWLINES=false during test task App.kt initializes JUL statically. CloudWatchFormatter.kt configures replace newlines by env var. resource-config.json fixes log file pattern. sam-local-env.json sets LOGGER_REPLACE_NEWLINES=false for sam local invoke AppTest.java removed, now kt file

--- HelloWorldFunction/src/main/kotlin/helloworld/CloudWatchFormatter.kt
+++ HelloWorldFunction/src/main/kotlin/helloworld/CloudWatchFormatter.kt
@@ -5,19 +5,39 @@ import java.util.logging.SimpleFormatter
 
 /**
  * CloudWatch has an issue where it sees every new line in a log message as a separate message.
- * This formatter replaces newlines with carriage returns so CloudWatch creates a single log record from a single
- * message.
+ * This checks if environment variable LOGGER_REPLACE_NEWLINES is false. Otherwise, the formatter replaces newlines with
+ * carriage returns so CloudWatch creates a single log record from a single message.
  * Created by Philip Yurchuk on 9/11/2022.
  */
 class CloudWatchFormatter : SimpleFormatter() {
 
+    companion object {
+
+        var replaceNewlines: Boolean = true
+
+        init {
+            try {
+                val envValue = System.getenv("LOGGER_REPLACE_NEWLINES")
+                System.out.println("LOGGER_REPLACE_NEWLINES = $envValue")
+                if (envValue.isNotBlank()) {
+                    replaceNewlines = envValue.toBoolean()
+                }
+            } catch (_: Exception) {
+            }
+            System.out.println("Replace newlines? $replaceNewlines")
+        }
+    }
+
     /**
-     * Replaces the log message's newlines with carriage returns before formatting.
+     * WARNING: any log statements in this method results in an infinite loop.
+     * If LOGGER_REPLACE_NEWLINES == true (default) replaces the log message's newlines with carriage returns before formatting.
      */
     override fun format(record: LogRecord): String {
 
-        val carriageReturnMsg = record.message.replace("\n", "\r") + "\n"
-        record.message = carriageReturnMsg
+        if (replaceNewlines) {
+            val carriageReturnMsg = record.message.replace("\n", "\r") + "\n"
+            record.message = carriageReturnMsg
+        }
         return super.format(record)
     }
 }
